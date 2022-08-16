d92b4f5c9864c7f869eb721e7ffaed03fc96071f
ErickFullStack1979 <e150979@hotmail.com>
2022-08-16 02:36:17 -0400

    inicio

---
+++ src/main/java/cl/darkos/Grupal06M6/model/dao/ICapacitacionRepository.java
@@ -0,0 +1,7 @@
+package cl.darkos.Grupal06M6.model.dao;
+
+import cl.darkos.Grupal06M6.model.entity.Capacitacion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ICapacitacionRepository extends CrudRepository<Capacitacion, Integer> {
+}
